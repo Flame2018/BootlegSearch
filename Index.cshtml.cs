3a237e90cb5088b2c6abd790613a403666416b2e
Flame2018 <54338887+Flame2018@users.noreply.github.com>
2019-10-17 18:47:14 -0500

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ShinobiRanks1._0.Data;
+
+namespace ShinobiRanks1._0.Pages.hvgenin
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ShinobiRanks1._0.Data.ApplicationDbContext _context;
+
+        public IndexModel(ShinobiRanks1._0.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<HVGenin> HVGenin { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            var vGenins = from m in _context.HVGenin select m;
+            vGenins = vGenins.Where(s => s.GeninName.Contains("Sasuke"));
+
+            HVGenin = await vGenins.ToListAsync();
+        }
+    }
+}
